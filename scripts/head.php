745b3053696ae6d7380b8ab5d7c5a5c52243c3e3
OliverSpeak <102995517+OliverSpeak@users.noreply.github.com>
2022-09-14 17:43:39 +0100

    Generic head - makes editing easier

---
+++ scripts/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="assets/favicons/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="assets/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="assets/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicons/apple-touch-icon.png">
+    <link href="styles/normalize.css" rel="stylesheet" type="text/css"/>
+    <link href="styles/style.css" rel="stylesheet" type="text/css"/>
+    <link href="styles/ie10_11.css" rel="stylesheet" type="text/css"/>
+    <script src="scripts/jquery-3.6.1.min.js"></script>
+    <script src="scripts/scripts.js"></script>
+    <noscript>
+        <style>
+          #bg {
+            display: inline !important; /* see style.css #bg */
+          }
+        </style>
+      </noscript>
